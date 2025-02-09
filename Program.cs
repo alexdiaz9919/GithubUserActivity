9be4bbbb70c68e08481e683cf61c836bd3a5472b
alexdiaz9919 <alexdiaz.99.19@gmail.com>
2025-02-09 12:34:10 -0600

    Agregar archivos de proyecto.

---
+++ Program.cs
@@ -0,0 +1,27 @@
+﻿using static System.Net.WebRequestMethods;
+
+namespace GithubUserActivity
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            if (args.Length != 2)
+                Console.WriteLine("Usage: github-activity <username>");
+            else
+            {
+                var username = args[1];
+                var url = $"https://api.github.com/users/{username}/events";
+
+                var client = new HttpClient();
+                client.BaseAddress = new Uri(url);
+
+                var response = client.GetAsync(url).Result;
+                response.EnsureSuccessStatusCode();
+
+                var result = response.Content.ReadAsStringAsync().Result;
+
+            }
+        }
+    }
+}
